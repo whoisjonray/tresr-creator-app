40a39281f9e8d5a244a54f1f307a257093946a5f
Jon Ray <whoisjonray@gmail.com>
2025-08-18 12:32:10 -0500

    fix: remove missing CSS import causing Railway build failure

--- client/src/pages/ProductManager.jsx
+++ client/src/pages/ProductManager.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
-import './ProductManager.css';
 import { userStorage } from '../utils/userStorage';
 import api from '../services/api';
 import { useAuth } from '../hooks/useAuth';
